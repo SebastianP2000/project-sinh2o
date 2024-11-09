6fe5cb4210159bd1379aa64263b0b48914e4e9b1
Andres <andr.silva@duocuc.cl>
2024-11-09 19:59:45 -0300

    Cambio de puerto de back

--- gestion-agricola/gestion-agricola-back/server.js
+++ gestion-agricola/gestion-agricola-back/server.js
@@ -87,8 +87,6 @@ mongoose.connect('mongodb+srv://sebpino:hR82oZwG1tl8tex4@cluster0.p7flg.mongodb.
 .catch(err => console.error('Error de conexión:', err));
 
 // Iniciar el servidor
-server.listen(PORT, () => { 
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+server.listen(process.env.PORT || 3000, () => { 
+    console.log(`http://localhost:${process.env.PORT || 3000}`);
 });
-
-
