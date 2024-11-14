2d53825a2ee225f1ea30b6e0bdd4994a4426cf8e
ElMoraHD <maestrorochi1999@gmail.com>
2024-11-14 17:34:52 -0300

    Cambios en modelos de sensor y cuadrante

--- gestion-agricola/gestion-agricola-back/models/Cuadrante.js
+++ gestion-agricola/gestion-agricola-back/models/Cuadrante.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const cuadranteSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
+    identificador: { type: String, required: true },
     ultima_irrigacion: { type: Date, default: Date.now  },
     fecha_sensor_agregado: { type: Date, default: Date.now  },
     tipo_planta: { type: String, required: true }
--- gestion-agricola/gestion-agricola-back/models/Sensores.js
+++ gestion-agricola/gestion-agricola-back/models/Sensores.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const sensorSchema = new mongoose.Schema({
-    sector: { type: String, required: true },
-    temperatura: { type: Number, required: true },
+    sensor_id: { type: String, required: true },
+    fecha_registro: { type: Date, default: Date.now },
     humedad: { type: Number, required: true },
-    estado: { type: Boolean, required: true, default: true },
-    fecha_registro: { type: Date, default: Date.now }
+    identificador: { type: String, required: true },
+    temperatura: { type: Number, required: true }
 });
 
 const sensores = mongoose.model('Sensores', sensorSchema);
